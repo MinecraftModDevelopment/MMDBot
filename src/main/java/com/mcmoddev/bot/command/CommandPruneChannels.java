8f6ca52be1ccaac28ef982e6201eb3bc76d5f0d1
Tyler Hancock <darklime@live.ca>
2016-12-14 21:06:00 -0700

    Prune list now uses embedded messages.

--- src/main/java/com/mcmoddev/bot/command/CommandPruneChannels.java
+++ src/main/java/com/mcmoddev/bot/command/CommandPruneChannels.java
@@ -7,19 +7,20 @@ import com.mcmoddev.bot.util.Utilities;
 
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.util.EmbedBuilder;
 
 public class CommandPruneChannels extends CommandAdmin {
     
     @Override
     public void processCommand (IMessage message, String[] params) {
         
-        try {
+        final LocalDateTime current = LocalDateTime.now();
+        final int minDaysOfInactivity = params.length == 2 ? Integer.parseInt(params[1]) : 7;
+        final StringBuilder builder = new StringBuilder();
+        
+        for (final IChannel channel : message.getGuild().getChannels()) {
             
-            final LocalDateTime current = LocalDateTime.now();
-            final int minDaysOfInactivity = params.length == 2 ? Integer.parseInt(params[1]) : 7;
-            final StringBuilder builder = new StringBuilder();
-            
-            for (final IChannel channel : message.getGuild().getChannels()) {
+            try {
                 
                 final IMessage latest = channel.getMessages().getLatestMessage();
                 
@@ -32,18 +33,24 @@ public class CommandPruneChannels extends CommandAdmin {
                 }
             }
             
-            Utilities.sendMessage(message.getChannel(), "The following channels have not been used in " + minDaysOfInactivity + " days." + Utilities.makeMultiCodeBlock(builder.toString()));
+            catch (ArrayIndexOutOfBoundsException e) {
+                
+                builder.append("#" + channel.getName() + " - unknown" + Utilities.SEPERATOR);
+            }
         }
         
-        catch (final ArrayIndexOutOfBoundsException e) {
-            
-            Utilities.sendMessage(message.getChannel(), "Exception reading channel messages. Has the bot had time to initialize fully?");
-        }
+        final EmbedBuilder embed = new EmbedBuilder();
+        embed.ignoreNullEmptyFields();
+        embed.withDesc(builder.toString());
+        embed.withColor((int) (Math.random() * 0x1000000));
+        embed.withFooterText("How do you work");
+        
+        Utilities.sendMessage(message.getChannel(), "The following channels have not been used in " + minDaysOfInactivity + " days.", embed.build());
     }
     
     @Override
     public String getDescription () {
         
         return "Lists all channels which have not been used in 7 days. You can change the amount of days to look for by adding a number to the end of the command.";
-    }   
+    }
 }
