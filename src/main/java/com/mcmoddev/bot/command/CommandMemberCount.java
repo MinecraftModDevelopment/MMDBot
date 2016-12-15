722b5057680a531fc74cd4fedb8d1bdf21abfff8
Tyler Hancock <darklime@live.ca>
2016-12-14 21:05:30 -0700

    Member count command can now break down numbers by roles.

--- src/main/java/com/mcmoddev/bot/command/CommandMemberCount.java
+++ src/main/java/com/mcmoddev/bot/command/CommandMemberCount.java
@@ -4,6 +4,8 @@ import com.mcmoddev.bot.util.Utilities;
 
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IMessage;
+import sx.blah.discord.handle.obj.IRole;
+import sx.blah.discord.util.EmbedBuilder;
 
 public class CommandMemberCount implements Command {
     
@@ -12,13 +14,33 @@ public class CommandMemberCount implements Command {
         
         final IGuild guild = message.getGuild();
         
-        if (guild != null)
-            Utilities.sendMessage(message.getChannel(), String.format("There are %d people in the server :)", guild.getUsers().size()));
+        if (guild != null) {
+            
+            final String base = String.format("There are %d people in the server :)", guild.getUsers().size());
+            
+            if (params.length == 2 && params[1].equalsIgnoreCase("roles")) {
+                
+                final StringBuilder builder = new StringBuilder();
+                
+                for (final IRole role : guild.getRoles())
+                    builder.append(String.format("%s: %d", role.getName().replaceAll("@", ""), guild.getUsersByRole(role).size()) + Utilities.SEPERATOR);
+                
+                final EmbedBuilder embed = new EmbedBuilder();
+                embed.ignoreNullEmptyFields();
+                embed.withDesc(builder.toString());
+                embed.withColor((int) (Math.random() * 0x1000000));
+                
+                Utilities.sendMessage(message.getChannel(), base, embed.build());
+            }
+            
+            else
+                Utilities.sendMessage(message.getChannel(), base);
+        }
     }
     
     @Override
     public String getDescription () {
         
-        return "Counts the amount of members in the room.";
+        return "Counts the number of members on the discord server. If roles is added as a parameter, a break down of members per role will also be given. ";
     }
 }
