9d2b0b6a201e7520410e440c1f7809bb09356a75
Tyler Hancock <darklime@live.ca>
2017-01-18 02:21:56 -0700

    Added a command to probe a channel for info.

---
+++ src/main/java/com/mcmoddev/bot/command/CommandProbe.java
@@ -0,0 +1,22 @@
+package com.mcmoddev.bot.command;
+
+import com.mcmoddev.bot.util.Utilities;
+
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
+
+public class CommandProbe extends CommandAdmin {
+    
+    @Override
+    public void processCommand (IMessage message, String[] params) {
+        
+        IChannel channel = message.getChannel();
+        Utilities.sendMessage(channel, Utilities.makeMultilineMessage("Channel: " + channel.getName(), "Topic: " + channel.getTopic(), "Position: " + channel.getPosition(), "Date: " + channel.getCreationDate().toLocalDate().toString(), "Users: " + channel.getUsersHere().size(), "ID: " + channel.getID()));
+    }
+    
+    @Override
+    public String getDescription () {
+        
+        return "Probes the current channel for info about it.";
+    }
+}
