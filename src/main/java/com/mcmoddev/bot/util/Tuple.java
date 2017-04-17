531013d0febae9312d4f2a064a081f1ae8293607
Tyler Hancock <darklime@live.ca>
2017-04-17 15:30:17 -0600

    Added a basic tuple class.

---
+++ src/main/java/com/mcmoddev/bot/util/Tuple.java
@@ -0,0 +1,12 @@
+package com.mcmoddev.bot.util;
+
+public class Tuple<X, Y> {
+
+    public final X x;
+    public final Y y;
+
+    public Tuple (X x, Y y) {
+        this.x = x;
+        this.y = y;
+    }
+}
