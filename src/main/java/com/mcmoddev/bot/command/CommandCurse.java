c652cda77039bfc167502078e11e4e526b9a96f4
Tyler Hancock <darklime@live.ca>
2017-09-06 23:37:57 -0600

    Update curse command to new system.

--- src/main/java/com/mcmoddev/bot/command/CommandCurse.java
+++ src/main/java/com/mcmoddev/bot/command/CommandCurse.java
@@ -1,95 +1,75 @@
 package com.mcmoddev.bot.command;
 
 import java.text.NumberFormat;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Map.Entry;
 
-import com.mcmoddev.bot.util.CurseData;
 import com.mcmoddev.bot.util.Utilities;
 
+import net.darkhax.cursedata.CurseData;
+import net.darkhax.cursedata.Member;
+import net.darkhax.cursedata.Project;
 import sx.blah.discord.handle.obj.IMessage;
 import sx.blah.discord.util.EmbedBuilder;
 
 public class CommandCurse implements Command {
 
-    // TOOD implement this
-    private static final Map<String, CurseData> CACHE = new HashMap<>();
+    private static NumberFormat nFormat = NumberFormat.getInstance();
 
     @Override
     public void processCommand (IMessage message, String[] params) {
 
-        final long time = System.currentTimeMillis();
+        Utilities.sendMessage(message.getChannel(), "Getting curse data for " + params[1]);
 
-        if (params.length >= 2) {
+        final StringBuilder builder = new StringBuilder();
+        final EmbedBuilder embed = new EmbedBuilder();
+        final Member member = CurseData.getMember(params[1]);
 
-            if (params[1].contains("@")) {
+        if (member.getUsername().equals("%INVALID%")) {
 
-                Utilities.sendMessage(message.getChannel(), "This command uses Curse names, not Discord ones!");
-                return;
-            }
-
-            Utilities.sendMessage(message.getChannel(), "Getting stats for " + params[1] + ", this will take a bit.");
-
-            final CurseData data = new CurseData(params[1]);
-            final StringBuilder builder = new StringBuilder();
-            final EmbedBuilder embed = new EmbedBuilder();
-
-            if (!data.exists()) {
-
-                Utilities.sendMessage(message.getChannel(), "No user could be found by the name " + params[1]);
-                return;
-            }
-
-            else if (!data.hasProjects()) {
-
-                Utilities.sendMessage(message.getChannel(), "No projects found for " + params[1]);
-                return;
-            }
-
-            int addedProjects = 0;
-            long otherDLs = 0;
-
-            for (final Entry<String, Long> set : data.getDownloads().entrySet()) {
-
-                if (addedProjects >= 10) {
-
-                    otherDLs += set.getValue();
-                    continue;
-                }
-
-                builder.append(set.getKey() + " - " + NumberFormat.getInstance().format(set.getValue()) + Utilities.SEPERATOR);
-                addedProjects++;
-            }
-
-            if (addedProjects < data.getProjectCount())
-                builder.append("Other Projects (" + (data.getProjectCount() - addedProjects) + ") - " + NumberFormat.getInstance().format(otherDLs) + Utilities.SEPERATOR);
-
-            builder.append("Total Projects: " + data.getProjectCount() + Utilities.SEPERATOR);
-
-            embed.ignoreNullEmptyFields();
-            embed.withDesc(builder.toString());
-            embed.withColor((int) (Math.random() * 0x1000000));
-            embed.withFooterText("Powered by Jared");
-            embed.withTitle("Total Downloads: " + NumberFormat.getInstance().format(data.getTotalDownloads()));
-
-            if (data.hasAvatar())
-                embed.withThumbnail(data.getAvatar());
-
-            System.out.println(embed.build().description.length());
-
-            Utilities.sendMessage(message.getChannel(), "Stats for " + params[1], embed.build());
+            Utilities.sendMessage(message.getChannel(), "No user could be found by the name " + params[1]);
+            return;
         }
 
-        else
-            Utilities.sendMessage(message.getChannel(), "You must specify the name of a user as well!");
+        else if (member.getProjects().size() == 0) {
 
-        System.out.println("Time: " + (System.currentTimeMillis() - time));
+            Utilities.sendMessage(message.getChannel(), "No projects found for " + params[1]);
+            return;
+        }
+
+        int addedProjects = 0;
+        long otherDLs = 0;
+        long total = 0;
+        long monthly = 0;
+
+        for (final Project project : member.getProjects()) {
+
+            if (addedProjects <= 10)
+                builder.append(Utilities.makeHyperlink(Utilities.sanatizeMarkdown(project.getTitle()), project.getProjectUrl()) + " - " + nFormat.format(project.getTotalDownloads()) + Utilities.SEPERATOR);
+            else
+                otherDLs += project.getTotalDownloads();
+
+            total += project.getTotalDownloads();
+            monthly += project.getMonthlyDownloads();
+            addedProjects++;
+        }
+
+        if (addedProjects > 10)
+            builder.append("Other Projects (" + (member.getProjects().size() - 10) + ") - " + nFormat.format(otherDLs) + Utilities.SEPERATOR);
+
+        builder.append("Total Projects: " + member.getProjects().size() + Utilities.SEPERATOR);
+        builder.append("Monthly Downloads: " + nFormat.format(monthly) + Utilities.SEPERATOR);
+
+        embed.ignoreNullEmptyFields();
+        embed.withDesc(builder.toString());
+        embed.withColor((int) (Math.random() * 0x1000000));
+        embed.withTitle("Total Downloads: " + nFormat.format(total) + " " + Utilities.getPercent(total, 1622770000L));
+        embed.withThumbnail(member.getAvatar());
+        Utilities.sendMessage(message.getChannel(), embed.build());
     }
 
     @Override
     public String getDescription () {
 
-        return "Gets the total number of downloads for a user on Curse. Stats are powered by Jared. This may take a few moments as we make him count them by hand.";
+        return null;
     }
+
 }
