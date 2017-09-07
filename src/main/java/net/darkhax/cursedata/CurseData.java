254f7786823414236586e7e7b70190f5a77ebbfd
Tyler Hancock <darklime@live.ca>
2017-09-06 23:35:36 -0600

    Finally commiting the new cursedata system.

---
+++ src/main/java/net/darkhax/cursedata/CurseData.java
@@ -0,0 +1,112 @@
+package net.darkhax.cursedata;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.jsoup.HttpStatusException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.google.gson.Gson;
+
+public class CurseData {
+
+    public static final Logger LOG = Logger.getLogger("CurseData");
+
+    public static final String PROJECTS_PAGE_URL = "https://mods.curse.com/members/%s/projects?page=%d";
+
+    public static final String WIDGET_URL = "https://widget.mcf.li%s.json";
+
+    public static final Gson gson = new Gson();
+
+    public static Project getProjectFromCurse (String curseId) {
+
+        return getProjectFromWidget(String.format(WIDGET_URL, curseId));
+    }
+
+    public static Project getProjectFromWidget (String widgetUrl) {
+
+        try {
+
+            return getProjectFromJson(getDocument(widgetUrl).body().text());
+        }
+
+        catch (final IOException e) {
+
+            LOG.info("Failed to read " + widgetUrl + " Retrying... " + e.getMessage());
+        }
+
+        return getProjectFromWidget(widgetUrl);
+    }
+
+    public static Project getProjectFromJson (String json) {
+
+        return gson.fromJson(json, Project.class);
+    }
+
+    public static Member getMember (String curseForgeName) {
+
+        int totalPages = 1;
+        String username = "%INVALID%";
+        String joined = "";
+        String avatar = "";
+        final List<Project> projects = new ArrayList<>();
+        final boolean isDone = false;
+
+        for (int page = 1; !isDone; page++) {
+            try {
+
+                final String docpage = String.format(PROJECTS_PAGE_URL, curseForgeName, page);
+                final Document doc = getDocument(docpage);
+
+                if (page == 1) {
+                    for (final Element pageElement : doc.getElementsByClass("b-pagination-item")) {
+                        final String text = pageElement.getElementsByClass("b-pagination-item").first().text();
+                        if (Character.isDigit(text.charAt(0)))
+                            if (text.length() <= String.valueOf(Integer.MAX_VALUE).length() && Integer.parseInt(text) > totalPages)
+                                totalPages = Integer.parseInt(text);
+                    }
+
+                    username = doc.getElementsByClass("username").first().text();
+                    joined = doc.getElementsByClass("joined").first().text();
+                    avatar = doc.getElementsByClass("avatar-100").first().getElementsByTag("img").first().attr("src");
+                }
+
+                final Element e = doc.getElementsByClass("project-listing").first();
+
+                for (final Element s : e.getElementsByTag("dt")) {
+
+                    final String projectId = s.getElementsByTag("a").first().attr("href");
+
+                    if (!projectId.startsWith("/mc-mods/minecraft/"))
+                        continue;
+                    final Project p = getProjectFromCurse(projectId);
+
+                    if (p != null)
+                        projects.add(p);
+                }
+            }
+
+            catch (final IOException exception) {
+
+                if (exception instanceof HttpStatusException && ((HttpStatusException) exception).getStatusCode() == 404)
+                    LOG.info(String.format("The cursename %s was invalid", curseForgeName));
+                else
+                    exception.printStackTrace();
+            }
+
+            if (page == totalPages)
+                break;
+        }
+
+        return new Member(username, avatar, joined, projects);
+    }
+
+    private static Document getDocument (String url) throws IOException {
+
+        return Jsoup.connect(url).ignoreContentType(true).userAgent("Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52").referrer("https://mods.curse.com").timeout(12000).ignoreHttpErrors(true).followRedirects(true).get();
+    }
+}
