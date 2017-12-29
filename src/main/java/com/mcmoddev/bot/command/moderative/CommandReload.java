740a06088146f3577ab906e509e7f438df95fec4
Tyler Hancock <darklime@live.ca>
2017-12-29 03:35:26 -0700

    Added a reload command.

---
+++ src/main/java/com/mcmoddev/bot/command/moderative/CommandReload.java
@@ -0,0 +1,24 @@
+package com.mcmoddev.bot.command.moderative;
+
+import net.darkhax.botbase.IDiscordBot;
+import net.darkhax.botbase.commands.CommandModerator;
+import net.darkhax.botbase.utils.MessageUtils;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IMessage;
+
+public class CommandReload extends CommandModerator {
+
+    @Override
+    public void processCommand (IDiscordBot bot, IChannel channel, IMessage message, String[] params) {
+
+        final long start = System.currentTimeMillis();
+        bot.reload();
+        MessageUtils.sendMessage(channel, "I am reloaded! Took " + (System.currentTimeMillis() - start) + "ms.");
+    }
+
+    @Override
+    public String getDescription () {
+
+        return "Causes the bot to reload.";
+    }
+}
