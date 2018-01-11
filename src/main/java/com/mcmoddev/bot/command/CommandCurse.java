f72bd06255f0d0d600f4d7cbed2947059b825c9d
Tyler Hancock <darklime@live.ca>
2018-01-11 00:04:35 -0700

    Updated existing commands to new system.

--- src/main/java/com/mcmoddev/bot/command/CommandCurse.java
+++ src/main/java/com/mcmoddev/bot/command/CommandCurse.java
@@ -1,15 +1,17 @@
 package com.mcmoddev.bot.command;
 
-import com.mcmoddev.bot.util.Utilities;
-
+import net.darkhax.botbase.IDiscordBot;
+import net.darkhax.botbase.commands.Command;
+import net.darkhax.botbase.utils.MessageUtils;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 
 public class CommandCurse implements Command {
 
     @Override
-    public void processCommand (IMessage message, String[] params) {
+    public void processCommand (IDiscordBot bot, IChannel channel, IMessage message, String[] params) {
 
-        Utilities.sendMessage(message.getChannel(), "This command is currently disabled. Stay tuned for updates.");
+        MessageUtils.sendMessage(message.getChannel(), "This command is currently disabled. Stay tuned for updates.");
     }
 
     @Override
--- src/main/java/com/mcmoddev/bot/command/CommandHelp.java
+++ src/main/java/com/mcmoddev/bot/command/CommandHelp.java
@@ -1,39 +1,16 @@
 package com.mcmoddev.bot.command;
 
-import java.util.Map.Entry;
-
-import com.mcmoddev.bot.MMDBot;
-import com.mcmoddev.bot.handlers.CommandHandler;
-import com.mcmoddev.bot.util.Utilities;
-
+import net.darkhax.botbase.IDiscordBot;
+import net.darkhax.botbase.commands.Command;
+import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 
 public class CommandHelp implements Command {
 
     @Override
-    public void processCommand (IMessage message, String[] args) {
+    public void processCommand (IDiscordBot bot, IChannel channel, IMessage message, String[] args) {
 
-        String descriptions = "";
-
-        if (args.length > 1) {
-            for (int index = 1; index < args.length; index++) {
-
-                final Command cmd = CommandHandler.getCommand(args[index]);
-
-                if (cmd != null && cmd.isValidUsage(message)) {
-                    descriptions += MMDBot.config.key + " " + args[index] + " - " + cmd.getDescription() + Utilities.SEPERATOR + Utilities.SEPERATOR;
-                }
-            }
-        }
-        else {
-            for (final Entry<String, Command> command : CommandHandler.getCommands().entrySet()) {
-                if (command.getValue().isValidUsage(message)) {
-                    descriptions += MMDBot.config.key + " " + command.getKey() + " - " + command.getValue().getDescription() + Utilities.SEPERATOR + Utilities.SEPERATOR;
-                }
-            }
-        }
-
-        Utilities.sendPrivateMessage(message.getAuthor(), Utilities.makeMultiCodeBlock(descriptions));
+        // TODO
     }
 
     @Override
