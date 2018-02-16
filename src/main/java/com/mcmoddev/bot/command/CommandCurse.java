47d8f4081a88db4ee7b82ebcb143f16acecbb5d9
Tyler Hancock <darklime@live.ca>
2018-02-16 15:05:05 -0700

    Added author name overrides for curse command.

--- src/main/java/com/mcmoddev/bot/command/CommandCurse.java
+++ src/main/java/com/mcmoddev/bot/command/CommandCurse.java
@@ -1,5 +1,10 @@
 package com.mcmoddev.bot.command;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.mcmoddev.bot.cursemeta.CurseMetaTracker;
 import com.mcmoddev.bot.cursemeta.MessageMods;
 
 import net.darkhax.botbase.BotBase;
@@ -10,19 +15,63 @@ import sx.blah.discord.handle.obj.IMessage;
 
 public class CommandCurse implements Command {
 
-    public static final String DAILY = "https://cursemeta.dries007.net/daily.json";
-    public static final String WEEKLY = "https://cursemeta.dries007.net/weekly.json";
-    public static final String MONTHLY = "https://cursemeta.dries007.net/monthly.json";
-
     @Override
     public void processCommand (BotBase bot, IChannel channel, IMessage message, String[] params) {
 
-        MessageUtils.sendMessage(channel, new MessageMods(10, params).build());
+        final Set<String> authors = new HashSet<>();
+
+        for (final String param : params) {
+
+            // The name "me" is treated as the discord name of the sender.
+            if (param.equalsIgnoreCase("me")) {
+
+                authors.add(message.getAuthor().getDisplayName(channel.getGuild()));
+            }
+
+            // The name "jarhax" is treated as Jared and Darkhax
+            else if (param.equalsIgnoreCase("jarhax")) {
+
+                authors.add("darkh4x");
+                authors.add("jaredlll08");
+            }
+
+            // Darkhax has a messed up curse account that has multiple names. Remap to the
+            // right one.
+            else if (param.equalsIgnoreCase("darkhax") || param.equals("darkhaxdev")) {
+
+                authors.add("darkh4x");
+            }
+
+            else if (param.equals("mmd")) {
+
+                authors.addAll(Arrays.asList(new String[] { "darkh4x", "jriwanek", "jamieswhiteshirt", "dshadowwolf", "lordcazsius", "morpheus1101", "poke1650", "proxyneko", "jaredlll08", "lclc98", "trentv4", "cleverpanda714", "pau101", "lewis_mcreu", "racerdelux", "boqzo1", "zaweri", "carstorm", "minecraftmoddevelopment", "spicephantom" }));
+            }
+
+            // Remaps the lemon emoji to lemonszz
+            else if (param.codePointAt(0) == 127819) {
+
+                authors.add("lemonszz");
+            }
+
+            // The name * is treated as all mod devs.
+            else if (param.equalsIgnoreCase("*")) {
+
+                authors.addAll(CurseMetaTracker.instance.getAuthors().keySet());
+            }
+
+            // The name has no overrides.
+            else {
+
+                authors.add(param.toLowerCase());
+            }
+        }
+
+        MessageUtils.sendMessage(channel, new MessageMods(10, authors.toArray(new String[0])).build());
     }
 
     @Override
     public String getDescription () {
 
-        return "Generates stats for a creator on Curse. Temporarily disabled.";
+        return "Prints out information about mod authors on Curse. You can list multiple author names, or use * for all mod authors.";
     }
 }
