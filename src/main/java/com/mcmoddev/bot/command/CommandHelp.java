229ad9043652bfbfee7b10162becb7fae778e245
Tyler Hancock <darklime@live.ca>
2018-02-16 15:04:35 -0700

    Cleaned up the help command a bit.

--- src/main/java/com/mcmoddev/bot/command/CommandHelp.java
+++ src/main/java/com/mcmoddev/bot/command/CommandHelp.java
@@ -1,7 +1,11 @@
 package com.mcmoddev.bot.command;
 
+import java.util.Map.Entry;
+import java.util.StringJoiner;
+
 import net.darkhax.botbase.BotBase;
 import net.darkhax.botbase.commands.Command;
+import net.darkhax.botbase.utils.MessageUtils;
 import sx.blah.discord.handle.obj.IChannel;
 import sx.blah.discord.handle.obj.IMessage;
 
@@ -10,11 +14,22 @@ public class CommandHelp implements Command {
     @Override
     public void processCommand (BotBase bot, IChannel channel, IMessage message, String[] args) {
 
+        final StringJoiner joiner = new StringJoiner(MessageUtils.SEPERATOR + MessageUtils.SEPERATOR);
+
+        for (final Entry<String, Command> s : bot.getCommands().getCommands().entrySet()) {
+
+            if (s.getValue().isValidUsage(bot, message)) {
+
+                joiner.add(bot.getCommandKey() + " " + s.getKey() + " - " + s.getValue().getDescription());
+            }
+        }
+
+        MessageUtils.sendPrivateMessage(bot.instance, message.getAuthor(), MessageUtils.makeMultiCodeBlock(joiner.toString()));
     }
 
     @Override
     public String getDescription () {
 
-        return "Lists all commands available to the user, along with a basic description of each command. You can run the command with other command names as additional arguments to get a more thorough description of the command.";
+        return "Lists all commands available to the user, along with a basic description of each command.";
     }
 }
