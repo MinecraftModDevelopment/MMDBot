461dbd98ea811eac72ecf66faf740a9d6f9cfe97
Jared <jaredlll08@gmail.com>
2018-02-20 00:15:32 +0200

    Added Mod Command

---
+++ src/main/java/com/mcmoddev/bot/cursemeta/MessageMod.java
@@ -0,0 +1,103 @@
+package com.mcmoddev.bot.cursemeta;
+
+import net.darkhax.botbase.utils.MessageUtils;
+import sx.blah.discord.util.EmbedBuilder;
+
+import java.text.NumberFormat;
+import java.util.*;
+
+public class MessageMod extends EmbedBuilder {
+
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getNumberInstance(Locale.US);
+
+    private final List<ModInfo> sortedMods;
+    private final int modsToShow;
+    private final int hiddenMods;
+
+    private long totalDownloads;
+    private long totalMonthlyDownloads;
+    private long hiddenDownloads;
+
+    public MessageMod(int modsToShow, String... mods) {
+
+        super();
+
+        final String modNames = Arrays.toString(mods);
+
+        this.withTitle(modNames.length() < 250 ? modNames : mods.length + " mods");
+        this.sortedMods = this.getMods(mods);
+        this.modsToShow = modsToShow;
+        this.hiddenMods = Math.max(0, this.sortedMods.size() - modsToShow);
+
+        this.setLenient(true);
+        this.setDownloadInfo();
+
+        final Random rand = new Random();
+        this.withColor(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255));
+    }
+
+    private void setDownloadInfo () {
+
+        if (this.sortedMods.isEmpty()) {
+
+            this.withDesc("No mods were found. Make sure mod names match those on CurseForge.");
+            return;
+        }
+
+        int modCount = 0;
+        final StringJoiner projectText = new StringJoiner(MessageUtils.SEPERATOR);
+
+        for (final ModInfo mod : this.sortedMods) {
+
+            this.totalDownloads += mod.getDownloads();
+            this.totalMonthlyDownloads += mod.getDownloadsMonthly();
+            
+            if (modCount < this.modsToShow) {
+
+                modCount++;
+                projectText.add(MessageUtils.makeHyperlink(mod.getName(), mod.getProjectPage()) + " - " + NUMBER_FORMAT.format(mod.getDownloads()));
+            }
+
+            else {
+
+                this.hiddenDownloads += mod.getDownloads();
+            }
+        }
+
+        if (this.hiddenMods > 0) {
+
+            projectText.add(String.format("Other Mods (%d) - %s", this.hiddenMods, NUMBER_FORMAT.format(this.hiddenDownloads)));
+        }
+
+        projectText.add(" ");
+        projectText.add(MessageUtils.makeBold("Total Downloads: ") + NUMBER_FORMAT.format(this.totalDownloads));
+        projectText.add("Total Mods: " + this.sortedMods.size());
+        projectText.add("Monthly Downloads: " + NUMBER_FORMAT.format(this.totalMonthlyDownloads));
+
+        final float percentage = (float) this.totalDownloads / (float) CurseMetaTracker.instance.getAllDownloads() * 100f;
+
+        if (percentage >= 0.01) {
+
+            projectText.add(String.format("Percentage of all downloads: %.2f", percentage) + "%");
+        }
+
+        this.withDesc(projectText.toString());
+    }
+
+    private List<ModInfo> getMods(String... modNames) {
+
+        final Set<ModInfo> mods = new HashSet<>();
+        List<String> modList = Arrays.asList(modNames);
+        for(Map.Entry<Long, ModInfo> entry : CurseMetaTracker.instance.getMods().entrySet()) {
+            if(modList.contains(entry.getValue().getName().toLowerCase().replaceAll(" ", "-"))){
+                mods.add(entry.getValue());
+            }else if(modList.contains(entry.getKey().toString())){
+                mods.add(entry.getValue());
+            }
+        }
+
+        final List<ModInfo> sorted = new ArrayList<>(mods);
+        Collections.sort(sorted, Collections.reverseOrder());
+        return sorted;
+    }
+}
