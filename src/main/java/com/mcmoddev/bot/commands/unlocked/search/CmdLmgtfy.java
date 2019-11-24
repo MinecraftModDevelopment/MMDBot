8d38606cfc2267106bdbe858a99e754d74a85c3f
Jasmine Iwanek <jasmine@iwanek.co.uk>
2019-11-24 07:53:01 -0800

    Add some additional options
    None are enabled yet, just here for notes right now

--- src/main/java/com/mcmoddev/bot/commands/unlocked/search/CmdLmgtfy.java
+++ src/main/java/com/mcmoddev/bot/commands/unlocked/search/CmdLmgtfy.java
@@ -25,8 +25,19 @@ public final class CmdLmgtfy extends Command {
     /**
      *
      */
+    @SuppressWarnings("unused")
     @Override
     protected void execute(final CommandEvent event) {
+	final String preview = "&pp=1"; // Use the preview page (For debug only)
+        final String google = "";
+        final String lmgtfy = "&s=l";
+        final String yahoo = "&s=y";
+        final String bing = "&s=b";
+        final String startpage = "&s=t";
+        final String ask = "&s=k";
+        final String aol = "&s=a";
+        final String duckduckgo = "&s=d";
+	final String explainer = "&iie=1"; // Include the internet Explainer
         final TextChannel channel = event.getTextChannel();
         final String searchTerm = event.getMessage().getContentRaw().toLowerCase(Locale.ENGLISH)
         		.replace(MMDBot.getConfig().getPrefix() + "lmgtfy", "");
