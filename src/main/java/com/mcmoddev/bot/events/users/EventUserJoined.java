ca3cd3c2186798c561905904db0bc21998eb6c9c
williambl <william@williambl.com>
2020-01-27 21:09:19 +0000

    Add logging if a member cannot be given their roles

--- src/main/java/com/mcmoddev/bot/events/users/EventUserJoined.java
+++ src/main/java/com/mcmoddev/bot/events/users/EventUserJoined.java
@@ -3,8 +3,10 @@ package com.mcmoddev.bot.events.users;
 import com.mcmoddev.bot.MMDBot;
 import com.mcmoddev.bot.misc.Utils;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.exceptions.HierarchyException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 import java.awt.*;
@@ -31,7 +33,18 @@ public final class EventUserJoined extends ListenerAdapter {
 
         if (MMDBot.getConfig().getGuildID().equals(guildId)) {
             final List<Role> roles = Utils.getOldUserRoles(guild, user.getIdLong());
-            roles.forEach(role -> guild.addRoleToMember(member, role).queue());
+            if (roles != null && member != null) {
+				for (Role role : roles) {
+					try {
+						guild.addRoleToMember(member, role).queue();
+					} catch (final HierarchyException e) {
+						MMDBot.LOGGER.info("Unable to give member %s role %s: %s", member.getId(), role.getId(), e.getMessage());
+						final Message consoleMessage = new MessageBuilder().appendFormat("Unable to give member %s role %s: %s", member.getAsMention(), role.getAsMention(), e.getMessage()).build();
+						final TextChannel consoleChannel = guild.getTextChannelById(MMDBot.getConfig().getChannelIDConsole());
+						if (consoleChannel != null) consoleChannel.sendMessage(consoleMessage).queue();
+					}
+				}
+			}
             embed.setColor(Color.GREEN);
             embed.setTitle("User Joined");
             embed.setThumbnail(user.getEffectiveAvatarUrl());
