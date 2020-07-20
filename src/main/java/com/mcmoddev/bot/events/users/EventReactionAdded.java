78c0c5222043e2bc8dd95b9d717c1e31243845af
williambl <william@williambl.com>
2020-07-20 23:13:21 +0100

    Make bot only warn users for bad requests once.

--- src/main/java/com/mcmoddev/bot/events/users/EventReactionAdded.java
+++ src/main/java/com/mcmoddev/bot/events/users/EventReactionAdded.java
@@ -7,10 +7,15 @@ import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.react.MessageReactionAddEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  */
 public final class EventReactionAdded extends ListenerAdapter {
+
+	private Set<Message> warnedMessages = new HashSet<>();
 
 	/**
 	 *
@@ -50,9 +55,11 @@ public final class EventReactionAdded extends ListenerAdapter {
 				responseBuilder.appendFormat("It received %d 'bad' reactions, %d 'needs improvement' reactions, and %d 'good' reactions.",
 					badReactions, needsImprovementReactions, goodReactions);
 
+				warnedMessages.remove(message);
+
 				if (discussionChannel == null) return;
 				discussionChannel.sendMessage(responseBuilder.build()).queue();
-			} else if ((badReactions + needsImprovementReactions * 0.5) - goodReactions >= MMDBot.getConfig().getWarningBadReactionThreshold()) {
+			} else if (!warnedMessages.contains(message) && (badReactions + needsImprovementReactions * 0.5) - goodReactions >= MMDBot.getConfig().getWarningBadReactionThreshold()) {
 				final MessageBuilder responseBuilder = new MessageBuilder();
 				responseBuilder.append(messageAuthor.getAsMention());
 				responseBuilder.append(", ");
@@ -60,6 +67,8 @@ public final class EventReactionAdded extends ListenerAdapter {
 					"Please edit your message to bring it to a higher standard.\n");
 				responseBuilder.appendFormat("It has so far received %d 'bad' reactions, %d 'needs improvement' reactions, and %d 'good' reactions.",
 					badReactions, needsImprovementReactions, goodReactions);
+
+				warnedMessages.add(message);
 
 				if (discussionChannel == null) return;
 				discussionChannel.sendMessage(responseBuilder.build()).queue();
