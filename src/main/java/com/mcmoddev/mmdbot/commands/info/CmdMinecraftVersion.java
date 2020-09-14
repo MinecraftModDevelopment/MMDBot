7d27e49802bc3aad27c97b6cd0ea9382ec23dc9f
williambl <william@williambl.com>
2020-09-14 19:03:24 +0100

    Add Minecraft update tracking

---
+++ src/main/java/com/mcmoddev/mmdbot/commands/info/CmdMinecraftVersion.java
@@ -0,0 +1,45 @@
+package com.mcmoddev.mmdbot.commands.info;
+
+import com.jagrosh.jdautilities.command.Command;
+import com.jagrosh.jdautilities.command.CommandEvent;
+import com.mcmoddev.mmdbot.core.Utils;
+import com.mcmoddev.mmdbot.updatenotifiers.forge.ForgeVersionHelper;
+import com.mcmoddev.mmdbot.updatenotifiers.forge.MinecraftForgeVersion;
+import com.mcmoddev.mmdbot.updatenotifiers.game.MinecraftVersionHelper;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.TextChannel;
+
+import java.awt.*;
+import java.time.Instant;
+
+/**
+ *
+ */
+public final class CmdMinecraftVersion extends Command {
+
+	/**
+	 *
+	 */
+	public CmdMinecraftVersion() {
+		super();
+		this.name = "minecraftv";
+		aliases = new String[]{"minecraft", "mcv"};
+		help = "Get the latest Minecraft versions";
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	protected void execute(final CommandEvent event) {
+		final EmbedBuilder embed = new EmbedBuilder();
+		final TextChannel channel = event.getTextChannel();
+
+		embed.setTitle("Minecraft versions");
+		embed.addField("Latest", MinecraftVersionHelper.getLatest(), true);
+		embed.addField("Latest Stable", MinecraftVersionHelper.getLatestStable(), true);
+		embed.setColor(Color.GREEN);
+		embed.setTimestamp(Instant.now());
+		channel.sendMessage(embed.build()).queue();
+	}
+}
