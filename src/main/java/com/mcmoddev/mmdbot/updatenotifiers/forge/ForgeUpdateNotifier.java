dc40e3c62d531f298fc79fe1fdace2e6bcab665c
williambl <william@williambl.com>
2020-09-24 17:34:53 +0100

    Improve forge update embed

--- src/main/java/com/mcmoddev/mmdbot/updatenotifiers/forge/ForgeUpdateNotifier.java
+++ src/main/java/com/mcmoddev/mmdbot/updatenotifiers/forge/ForgeUpdateNotifier.java
@@ -30,28 +30,32 @@ public class ForgeUpdateNotifier extends TimerTask {
             ForgeVersion latest = ForgeVersionHelper.getForgeVersionsForMcVersion(mcVersion);
 
             boolean changed = false;
-            StringBuilder body = new StringBuilder(String.format("Minecraft version: **%s**\n", mcVersion));
+            EmbedBuilder embed = new EmbedBuilder();
+			embed.addField("Minecraft Version", mcVersion, true);
+			embed.setTitle("Forge version update");
+			embed.setColor(Color.ORANGE);
+			embed.setTimestamp(Instant.now());
 
-            if (latest.getLatest() != null) {
+			if (latest.getLatest() != null) {
                 if (lastForgeVersions.getLatest() == null) {
-                    body.append(String.format("Latest: **none** -> **%s**\n", latest.getLatest()));
-                    body.append(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getLatest())));
+					embed.addField("Latest", String.format("*none* -> **%s**\n", latest.getLatest()), true);
+					embed.setDescription(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getLatest())));
                     changed = true;
                 } else if (!latest.getLatest().equals(lastForgeVersions.getLatest())) {
-                    body.append(String.format("Latest: **%s** -> **%s**\n", lastForgeVersions.getLatest(), latest.getLatest()));
-                    body.append(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getLatest())));
+                    embed.addField("Latest", String.format("**%s** -> **%s**\n", lastForgeVersions.getLatest(), latest.getLatest()), true);
+                    embed.setDescription(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getLatest())));
                     changed = true;
                 }
             }
 
             if (latest.getRecommended() != null) {
                 if (lastForgeVersions.getRecommended() == null) {
-                    body.append(String.format("Recommended: **none** -> **%s**\n", latest.getRecommended()));
-                    body.append(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getRecommended())));
+                    embed.addField("Recommended", String.format("*none* -> **%s**\n", latest.getRecommended()), true);
+                    embed.setDescription(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getRecommended())));
                     changed = true;
                 } else if (!latest.getRecommended().equals(lastForgeVersions.getRecommended())) {
-                    body.append(String.format("Recommended: **%s** -> **%s**\n", lastForgeVersions.getRecommended(), latest.getRecommended()));
-                    body.append(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getRecommended())));
+                    embed.addField("Recommended", String.format("**%s** -> **%s**\n", lastForgeVersions.getRecommended(), latest.getRecommended()), true);
+                    embed.setDescription(Utils.makeHyperlink("Changelog", String.format(CHANGELOG_URL_TEMPLATE, mcVersion, latest.getRecommended())));
                     changed = true;
                 }
             }
@@ -60,15 +64,10 @@ public class ForgeUpdateNotifier extends TimerTask {
                 // TODO: save this to disk to persist on restarts
                 lastForgeVersions = latest;
 
-                final EmbedBuilder embed = new EmbedBuilder();
                 Long guildId = MMDBot.getConfig().getGuildID();
                 final Guild guild = MMDBot.getInstance().getGuildById(guildId);
                 Long channelId = MMDBot.getConfig().getChannelIDForgeNotifier();
                 final TextChannel channel = guild.getTextChannelById(channelId);
-                embed.setTitle("Forge version update");
-                embed.setDescription(body.toString());
-                embed.setColor(Color.ORANGE);
-                embed.setTimestamp(Instant.now());
                 channel.sendMessage(embed.build()).queue();
             } else {
                 MMDBot.LOGGER.info("[ForgeUpdateNotifier] No Forge version update");
