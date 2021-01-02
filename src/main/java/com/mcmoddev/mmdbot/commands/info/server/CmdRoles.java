b6233dbe3b204e581cebcbabb12c6ec7d79fb831
sciwhiz12 <arnoldnunag12@gmail.com>
2021-01-02 10:40:24 +0800

    Clean up roles command class, use mentions

--- src/main/java/com/mcmoddev/mmdbot/commands/info/server/CmdRoles.java
+++ src/main/java/com/mcmoddev/mmdbot/commands/info/server/CmdRoles.java
@@ -12,14 +12,15 @@ import java.awt.Color;
 import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
- *
+ * A {@link Command} for listing the roles and their member counts.
  */
 public final class CmdRoles extends Command {
 
     /**
-     *
+     * Constructs this command, to be registered in a {@link com.jagrosh.jdautilities.command.CommandClient}.
      */
     public CmdRoles() {
         super();
@@ -29,18 +30,12 @@ public final class CmdRoles extends Command {
     }
 
     /**
-     * Get the amount of members per role.
-     *
-     * @param guild The guild we are in.
-     * @param id    The ID of the role.
-     * @return
-     */
-    private String getRoleMemberCount(final Guild guild, final String id) {
-        return Integer.toString(guild.getMembersWithRoles(guild.getRoleById(id)).size());
-    }
-
-    /**
-     *
+     * Executes the command.
+	 * <p>
+	 * Sends a message with a listing of all roles in the guild, along with a count of how many members hold those roles.
+	 * The message is sent in the same channel where the command was sent from.
+	 *
+	 * @param event The command event
      */
     @Override
     protected void execute(final CommandEvent event) {
@@ -49,20 +44,16 @@ public final class CmdRoles extends Command {
         final EmbedBuilder embed = new EmbedBuilder();
         final TextChannel channel = event.getTextChannel();
 
-        for (Role roles : event.getGuild().getRoles()) {
-            try {
-                if (!roleList.containsKey(roles.getName())) {
-                    roleList.put(roles.getName(), getRoleMemberCount(guild, roles.getId()));
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
+        final String rolesCount = event.getGuild().getRoles()
+			.stream()
+			.filter(role -> !role.isManaged())
+			.map(role -> role.getAsMention() + ": " + role.getGuild().getMembersWithRoles(role).size())
+			.collect(Collectors.joining("\n"));
 
         embed.setColor(Color.GREEN);
         embed.setTitle("Users With Role");
         embed.setDescription("A count of how many members have been assigned some of MMD's many roles.");
-        embed.addField("Role count:", Utils.mapToString(Utils.sortByValue(roleList, true)), true);
+        embed.addField("Role count:", rolesCount, true);
         embed.setTimestamp(Instant.now());
         channel.sendMessage(embed.build()).queue();
     }
