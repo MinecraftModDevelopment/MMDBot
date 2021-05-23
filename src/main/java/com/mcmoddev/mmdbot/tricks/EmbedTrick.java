f672aec3bdcefc7c23a6555d1407669f40ab352d
williambl <william@williambl.com>
2021-05-23 21:16:46 +0100

    Add Embed Tricks

---
+++ src/main/java/com/mcmoddev/mmdbot/tricks/EmbedTrick.java
@@ -0,0 +1,58 @@
+package com.mcmoddev.mmdbot.tricks;
+
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.MessageBuilder;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EmbedTrick implements Trick {
+    private final List<String> names;
+    private final String title;
+    private final String description;
+    private final int color;
+    private final List<MessageEmbed.Field> fields;
+
+    public EmbedTrick(final List<String> names, final String title, final String description, final int color, final MessageEmbed.Field... fields) {
+        this.names = names;
+        this.title = title;
+        this.description = description;
+        this.color = color;
+        this.fields = Arrays.asList(fields);
+    }
+
+    @Override
+    public List<String> getNames() {
+        return names;
+    }
+
+    @Override
+    public Message getMessage(final String[] args) {
+        EmbedBuilder builder = new EmbedBuilder()
+            .setTitle(getTitle())
+            .setDescription(getDescription())
+            .setColor(color);
+        for (MessageEmbed.Field field : getFields()) {
+            builder.addField(field);
+        }
+        return new MessageBuilder(builder.build()).build();
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public List<MessageEmbed.Field> getFields() {
+        return fields;
+    }
+}
