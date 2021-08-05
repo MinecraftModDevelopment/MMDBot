3e903e93178972d83194781be11abd9dcd6bcee3
williambl <william@williambl.com>
2021-08-05 16:16:56 +0100

    Make the pagination actually work

--- src/main/java/com/mcmoddev/mmdbot/modules/commands/info/CmdMappings.kt
+++ src/main/java/com/mcmoddev/mmdbot/modules/commands/info/CmdMappings.kt
@@ -15,7 +15,6 @@ import net.dv8tion.jda.api.events.interaction.ButtonClickEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.components.Button
 import java.util.*
-import java.util.concurrent.Executors
 
 class CmdMappings(name: String, private val namespace: Namespace, vararg aliases: String?) : Command() {
     init {
@@ -39,110 +38,107 @@ class CmdMappings(name: String, private val namespace: Namespace, vararg aliases
         val query = args[0];
         val version = args.getOrElse(1) { namespace.getDefaultVersion() }
 
-        thread.use { ctx ->
-            runBlocking {
-                launch(ctx) {
-                    val provider = namespace.getProvider(version)
-                    var hasPerfectMatch = false
-                    val embeds = (MappingsQuery.queryClasses(
-                        QueryContext(
-                            provider,
-                            query
-                        )
-                    ).value.asSequence() + MappingsQuery.queryMember(
-                        QueryContext(provider, query)
-                    ) { it.members.asSequence() }.value.asSequence())
-                        .sortedBy { it.score }
-                        .also { seq ->
-                            hasPerfectMatch = hasPerfectMatch || seq.any { it.score == 1.0 }
-                        }
-                        .filter { if (hasPerfectMatch) it.score == 1.0 else true }
-                        .map {
-                            async(ctx) {
-                                @Suppress("UNCHECKED_CAST")
-                                when (it.value) {
-                                    is Class -> {
-                                        val value = it.value as Class
-                                        EmbedBuilder()
-                                            .setTitle("Yarn Class mapping for $version:")
-                                            .run {
-                                                if (value.mappedName != null)
-                                                    addField("Mapped Name", "`${value.mappedName}`", false)
-                                                else this
-                                            }
-                                            .addField("Intermediary/SRG Name", "`${value.intermediaryName}`", false)
-                                            .addField("Obfuscated Name", "`${value.obfName.merged}`", false)
-                                            .build()
-                                    }
-                                    is Pair<*, *> -> {
-                                        val value = it.value as Pair<Class, MappingsMember>
-                                        EmbedBuilder()
-                                            .setTitle(
-                                                "Yarn ${
-                                                    when (value.second) {
-                                                        is Field -> "Field"
-                                                        is Method -> "Method"
-                                                        else -> "Member"
-                                                    }
-                                                } mapping for $version:"
-                                            )
-                                            .addField("Mapped Name", "`${value.second.mappedName}`", false)
-                                            .addField(
-                                                "Intermediary/SRG Name",
-                                                "`${value.second.intermediaryName}`",
-                                                false
-                                            )
-                                            .addField("Obfuscated Name", "`${value.second.obfName.merged}`", false)
-                                            .addField(
-                                                "Member of Class",
-                                                "`${value.first.mappedName ?: value.first.intermediaryName}`",
-                                                false
-                                            )
-                                            .addField(
-                                                "Descriptor",
-                                                "`${value.second.getMappedDesc(provider.get())}`",
-                                                false
-                                            )
-                                            .build()
-                                    }
-                                    else -> {
-                                        EmbedBuilder().setDescription("???").build()
-                                    }
-                                }
-                            }
-                        }.iterator()
-
-                    val msg = event.channel.sendMessageEmbeds(embeds.next().await()).setActionRow(
-                        Button.primary("next", "Next")
-                    ).complete()
-
-                    ButtonListener.embedsForMessage[msg.idLong] = embeds
-                    delay(180000L)
-                    ButtonListener.embedsForMessage.remove(msg.idLong)
-                    event.channel.editMessageById(msg.id, msg).setActionRows().complete()
+        scope.launch {
+            val provider = namespace.getProvider(version)
+            var hasPerfectMatch = false
+            val embeds = (MappingsQuery.queryClasses(
+                QueryContext(
+                    provider,
+                    query
+                )
+            ).value.asSequence() + MappingsQuery.queryMember(
+                QueryContext(provider, query)
+            ) { it.members.asSequence() }.value.asSequence())
+                .sortedBy { it.score }
+                .also { seq ->
+                    hasPerfectMatch = hasPerfectMatch || seq.any { it.score == 1.0 }
                 }
-            }
+                .filter { if (hasPerfectMatch) it.score == 1.0 else true }
+                .mapIndexed { idx, it ->
+                    async {
+                        @Suppress("UNCHECKED_CAST")
+                        when (it.value) {
+                            is Class -> {
+                                val value = it.value as Class
+                                EmbedBuilder()
+                                    .setTitle("Yarn Class mapping for $version:")
+                                    .run {
+                                        if (value.mappedName != null)
+                                            addField("Mapped Name", "`${value.mappedName}`", false)
+                                        else this
+                                    }
+                                    .addField("Intermediary/SRG Name", "`${value.intermediaryName}`", false)
+                                    .addField("Obfuscated Name", "`${value.obfName.merged}`", false)
+                            }
+                            is Pair<*, *> -> {
+                                val value = it.value as Pair<Class, MappingsMember>
+                                EmbedBuilder()
+                                    .setTitle(
+                                        "Yarn ${
+                                            when (value.second) {
+                                                is Field -> "Field"
+                                                is Method -> "Method"
+                                                else -> "Member"
+                                            }
+                                        } mapping for $version:"
+                                    )
+                                    .addField("Mapped Name", "`${value.second.mappedName}`", false)
+                                    .addField(
+                                        "Intermediary/SRG Name",
+                                        "`${value.second.intermediaryName}`",
+                                        false
+                                    )
+                                    .addField("Obfuscated Name", "`${value.second.obfName.merged}`", false)
+                                    .addField(
+                                        "Member of Class",
+                                        "`${value.first.mappedName ?: value.first.intermediaryName}`",
+                                        false
+                                    )
+                                    .addField(
+                                        "Descriptor",
+                                        "`${value.second.getMappedDesc(provider.get())}`",
+                                        false
+                                    )
+                            }
+                            else -> {
+                                EmbedBuilder().setDescription("???")
+                            }
+                        }.setFooter("Page ${idx + 1} | Powered by linkie-core").build()
+                    }
+                }.iterator()
+
+            val msg = event.channel.sendMessageEmbeds(embeds.next().await()).apply {
+                if (embeds.hasNext()) {
+                    setActionRow(Button.primary("next", "Next"))
+                }
+            }.complete()
+
+            ButtonListener.embedsForMessage[msg.idLong] = embeds
+
+            delay(180000L)
+
+            ButtonListener.embedsForMessage.remove(msg.idLong)
+            event.channel.editMessageById(msg.id, msg).setActionRows().complete()
         }
     }
 
-
     companion object {
-        val mappings = LinkieConfig.DEFAULT.copy(namespaces=listOf(YarnNamespace, MCPNamespace))
-        val thread = Executors.newFixedThreadPool(2).asCoroutineDispatcher()
+        val mappings = LinkieConfig.DEFAULT.copy(namespaces = listOf(YarnNamespace, MCPNamespace))
+        val scope = CoroutineScope(Dispatchers.Default)
     }
 
-    object ButtonListener: ListenerAdapter() {
+    object ButtonListener : ListenerAdapter() {
         val embedsForMessage: MutableMap<Long, Iterator<Deferred<MessageEmbed>>> = mutableMapOf()
 
         override fun onButtonClick(event: ButtonClickEvent) {
-            thread.use { ctx ->
-                runBlocking {
-                    launch(ctx) {
-                        embedsForMessage[event.messageIdLong]?.let {
-                            val newEmbed = it.next().await()
-                            event.editMessageEmbeds(newEmbed).queue()
+            scope.launch {
+                embedsForMessage[event.messageIdLong]?.let {
+                    val newEmbed = it.next().await()
+                    event.editMessageEmbeds(newEmbed).apply {
+                        if (!it.hasNext()) {
+                            setActionRow()
                         }
-                    }
+                    }.queue()
                 }
             }
         }
