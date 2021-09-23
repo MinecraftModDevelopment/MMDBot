b6652156eb455372f0df7f868acc5efb36678279
Jasmine Iwanek <jasmine@iwanek.co.uk>
2021-09-22 20:50:39 -0400

    Some fixes to the module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,6 @@
-module MMDBot {
+module com.mcmoddev.mmdbot {
     exports com.mcmoddev.mmdbot;
 
-    requires annotations;
     requires com.electronwill.nightconfig.core;
     requires com.electronwill.nightconfig.toml;
     requires com.google.common;
@@ -9,10 +8,10 @@ module MMDBot {
     requires java.desktop;
     requires java.sql;
     requires java.xml;
-    requires jsr305;
-    requires linkie.core;
-    requires logback.classic;
-    requires logback.core;
+    requires kotlin.stdlib;
+//    requires kotlinx.coroutines.core.jvm;
+//    requires linkie.core;
+//    requires me.shedaniel.linkie;
     requires net.dv8tion.jda;
     requires org.flywaydb.core;
     requires org.jdbi.v3.core;
@@ -22,6 +21,4 @@ module MMDBot {
     requires org.xerial.sqlitejdbc;
     requires pw.chew.jdautilities.command;
     requires pw.chew.jdautilities.commons;
-    requires kotlin.stdlib;
-    requires kotlinx.coroutines.core.jvm;
 }
