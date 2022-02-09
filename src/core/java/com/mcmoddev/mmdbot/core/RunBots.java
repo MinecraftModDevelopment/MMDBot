91f18e24c377be2f69ecffeab9213c942cbc61f9
Matyrobbrt <matyrobbrt@gmail.com>
2022-02-09 19:30:34 +0200

    Update RunBots.java

--- src/core/java/com/mcmoddev/mmdbot/core/RunBots.java
+++ src/core/java/com/mcmoddev/mmdbot/core/RunBots.java
@@ -23,6 +23,7 @@ package com.mcmoddev.mmdbot.core;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonWriter;
 import com.ibm.icu.impl.Pair;
+import com.mcmoddev.mmdbot.core.bot.Bot;
 import com.mcmoddev.mmdbot.core.bot.BotRegistry;
 import com.mcmoddev.mmdbot.core.util.Constants;
 import org.slf4j.Logger;
@@ -46,12 +47,18 @@ public class RunBots {
         BotRegistry.getBotTypes()
             .entrySet()
             .stream()
-            .map(entry -> Pair.of(entry.getValue()
-                .createBot(createDirectory(getPathOrElse(config, entry.getKey(), Path.of(entry.getKey())))),
-                entry.getKey()))
+            .map(entry -> {
+                final var botEntry = BotEntry.of(entry.getKey(),
+                    config.has(entry.getKey()) ? config.get(entry.getKey()).getAsJsonObject() : new JsonObject());
+                return Pair.of(entry.getValue().createBot(createDirectory(Path.of(botEntry.runPath()))), botEntry);
+            })
             .forEach(botPair -> {
-                botPair.first.start();
-                botPair.first.getLogger().warn("Bot {} has been found, and it has been launched!", botPair.second);
+                if (botPair.second.isEnabled()) {
+                    botPair.first.start();
+                    botPair.first.getLogger().warn("Bot {} has been found, and it has been launched!", botPair.second.name());
+                } else {
+                    botPair.first.getLogger().warn("Bot {} is disabled! Its features will not work!", botPair.second.name());
+                }
             });
 
     }
@@ -82,8 +89,13 @@ public class RunBots {
                 try (final var fw = new BufferedWriter(new FileWriter(path.toFile()))) {
                     final var obj = new JsonObject();
                     BotRegistry.getBotTypes()
-                        .keySet().forEach(n -> obj.addProperty(n, n)); // Write all the known bots when generating it
-                    Constants.GSON.toJson(obj, new JsonWriter(fw));
+                        .keySet().forEach(n -> {
+                            final var botObj = new JsonObject();
+                            botObj.addProperty("enabled", true);
+                            botObj.addProperty("runPath", n);
+                            obj.add(n, botObj);
+                        }); // Write all the known bots when generating it
+                    Constants.GSON.toJson(obj, fw);
                 }
             } catch (IOException e) {
                 LOG.error("Exception while trying to generate config!", e);
@@ -95,6 +107,17 @@ public class RunBots {
             LOG.error("Exception while trying to read config!", e);
         }
         return new JsonObject();
+    }
+
+    private record BotEntry(String name, boolean isEnabled, String runPath) {
+        public static BotEntry of(String name, JsonObject json) {
+            if (json == null) {
+                json = new JsonObject();
+            }
+            final var enabled = json.has("enabled") && json.get("enabled").getAsBoolean();
+            final var runPath = json.has("runPath") ? json.get("runPath").getAsString() : name;
+            return new BotEntry(name, enabled, runPath);
+        }
     }
 
 }
