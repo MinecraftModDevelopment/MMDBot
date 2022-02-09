54bc5d079bb31c3d96048cedf4254193c704bbc8
Matyrobbrt <matyrobbrt@gmail.com>
2022-02-09 10:11:45 +0200

    Update fcw.js

--- tricks/fcw.js
+++ tricks/fcw.js
@@ -1,13 +1,11 @@
-let urlSuffix = ""
-if (args.length == 1) {
-	urlSuffix = parseString(args[0])
-} else if (args.length > 1) {
-	urlSuffix = `${args[0]}/${args[1]}`
-}
-const url = `https://forge.gemwire.uk/wiki/${urlSuffix}`
 const embed = new Embed()
+if (args.length < 1 || !args[0]) {
+	embed.setDescription(`There's a collection of information maintained by the community at https://forge.gemwire.uk that you might find useful.`)
+} else {
+	const url = `https://forge.gemwire.uk/wiki/${args.length > 1 ? (args[0] + "/" + args[1]) : args[0]}`
+	embed.setDescription(`There's an article at ${url} from the Forge Community Wiki that may be of interest to you.`)
+}
 embed.setTitle("Forge Community Wiki")
-embed.setDescription(`There's an article at ${url} from the Forge Community Wiki that may be of interest to you.`)
 embed.setFooter("FCW, hosted by Curle, managed by sciwhiz12")
 embed.setColor(parseInt(Math.random() * 16777215)) // Generate a random colour
 replyEmbed(embed)
