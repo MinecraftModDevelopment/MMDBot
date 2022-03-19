8e0208eb0426f4d86805a14fbd82194c62fa4165
Matyrobbrt <matyrobbrt@gmail.com>
2022-03-19 18:15:45 +0200

    Fix compile errors

--- src/main/java/com/mcmoddev/mmdbot/modules/logging/LoggingModule.java
+++ src/main/java/com/mcmoddev/mmdbot/modules/logging/LoggingModule.java
@@ -23,7 +23,6 @@ package com.mcmoddev.mmdbot.modules.logging;
 import com.mcmoddev.mmdbot.MMDBot;
 import com.mcmoddev.mmdbot.modules.logging.misc.EventReactionAdded;
 import com.mcmoddev.mmdbot.modules.logging.misc.ScamDetector;
-import com.mcmoddev.mmdbot.modules.logging.misc.ThreadChannelCreatorEvents;
 import com.mcmoddev.mmdbot.utilities.ThreadedEventListener;
 import com.mcmoddev.mmdbot.utilities.Utils;
 import com.mcmoddev.mmdbot.utilities.console.ConsoleChannelButtonListener;
