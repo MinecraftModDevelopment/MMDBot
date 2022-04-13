d34b9ed332b702ff83eaef528e5703635d2f059d
Matyrobbrt <matyrobbrt@gmail.com>
2022-04-13 16:36:41 +0300

    Fix TTT ties ending too early

--- src/commander/java/com/mcmoddev/mmdbot/commander/commands/games/TicTacToeCommand.java
+++ src/commander/java/com/mcmoddev/mmdbot/commander/commands/games/TicTacToeCommand.java
@@ -294,8 +294,8 @@ public class TicTacToeCommand extends SlashCommand {
     }
 
     private static boolean isEntireBoardUsed(final int[][] board) {
-        for (var x = 0; x < 2; x++) {
-            for (var y = 0; y < 2; y++) {
+        for (var x = 0; x < 3; x++) {
+            for (var y = 0; y < 3; y++) {
                 if (board[x][y] == UNKNOWN) return false;
             }
         }
