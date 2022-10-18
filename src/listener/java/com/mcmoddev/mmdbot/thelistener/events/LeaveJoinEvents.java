a156bb5d845208b134476b12e7c33ee75876f836
KiriCattus <kiri_cattus@icloud.com>
2022-10-18 23:53:47 +0100

    Smol correction...

--- src/listener/java/com/mcmoddev/mmdbot/thelistener/events/LeaveJoinEvents.java
+++ src/listener/java/com/mcmoddev/mmdbot/thelistener/events/LeaveJoinEvents.java
@@ -72,7 +72,7 @@ public final class LeaveJoinEvents extends ListenerAdapter {
             .setColor(Color.RED)
             .setTitle("User Left")
             .addField("User:", event.getUser().getAsTag(), true)
-            .addField("Roles", RoleEvents.mentionsOrEmpty(event.getMember().getRoles()), false)
+            .addField("Roles", RoleEvents.mentionsOrEmpty(event.getMember().getRoles()), true)
             .setFooter("User ID: " + event.getMember().getId(), event.getMember().getEffectiveAvatarUrl())
             .setTimestamp(Instant.now())
             .build();
